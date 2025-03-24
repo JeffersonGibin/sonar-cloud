2709bb1e49fd2c1e09ea73cac1430e1215d42799
Jefferson Gibin <jeffersongibin@yahoo.com.br>
2025-03-24 15:18:48 -0300

    comment test

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,7 +1,7 @@
-import { sum } from './lib.js';
+// import { sum } from './lib.js';
 
-describe('lib unit test', () => {
-    it('Should sum two numbers', () =>{
-        expect(sum(1, 2)).toBe(3);
-    });
-})
+// describe('lib unit test', () => {
+//     it('Should sum two numbers', () =>{
+//         expect(sum(1, 2)).toBe(3);
+//     });
+// })
