ee315653a4b091afdbfb857c99b583e6d304fedd
Jefferson Gibin <jeffersongibin@yahoo.com.br>
2025-03-24 15:41:00 -0300

    implement unit test

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,7 +1,11 @@
-import { sum } from './lib.js';
+import { sum, subtract } from './lib.js';
 
 describe('lib unit test', () => {
     it('Should sum two numbers', () =>{
         expect(sum(1, 2)).toBe(3);
+    });
+
+    it('Should subtract two numbers', () =>{
+        expect(subtract(3, 2)).toBe(1);
     });
 })
