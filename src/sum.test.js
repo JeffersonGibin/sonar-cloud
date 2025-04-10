07f880ad3ed98fec7bef119252375e043e284f47
Jefferson Gibin <jeffersongibin@yahoo.com.br>
2025-04-10 13:22:09 -0300

    force error sonar

--- src/sum.test.js
+++ src/sum.test.js
@@ -9,19 +9,4 @@ describe('lib unit test', () => {
         expect(subtract(3, 2)).toBe(1);
     });
 
-    it('Should divide two numbers', () =>{
-        expect(divide(3, 2)).toBe(1.5);
-    });
-
-    it('Should multiply two numbers', () =>{
-        expect(multiply(3, 2)).toBe(6);
-    });
-
-    it('Should power two numbers', () =>{
-        expect(power(3, 2)).toBe(9);
-    });
-
-    it('Should modulo two numbers', () =>{
-        expect(modulo(3, 2)).toBe(1);
-    });
 })
