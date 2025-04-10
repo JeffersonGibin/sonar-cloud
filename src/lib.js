ff628ad6dc8ccd397fc21966d1eb99ec6e7a3702
Jefferson Gibin <jeffersongibin@yahoo.com.br>
2025-04-10 13:13:29 -0300

    upgrade lib

--- src/lib.js
+++ src/lib.js
@@ -7,4 +7,21 @@ const subtract = (a, b) => {
     return a - b;
 };
 
-export { sum, subtract };
+const divide = (a, b) => {
+    return a / b;
+};
+
+const multiply = (a, b) => {
+    return a * b;
+};
+
+const power = (a, b) => {
+    return a ** b;
+};
+
+const modulo = (a, b) => {
+    return a % b;
+};
+
+
+export { sum, subtract, divide, multiply, power, modulo };
