c385cdd4d1fae37002456b5a311954b2ca52f3fb
Jefferson Gibin <jeffersongibin@yahoo.com.br>
2025-04-10 13:16:57 -0300

    add new tests for lib

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,4 +1,4 @@
-import { sum, subtract } from './lib.js';
+import { sum, subtract, divide, multiply, power, modulo } from './lib.js';
 
 describe('lib unit test', () => {
     it('Should sum two numbers', () =>{
@@ -7,5 +7,21 @@ describe('lib unit test', () => {
 
     it('Should subtract two numbers', () =>{
         expect(subtract(3, 2)).toBe(1);
+    });
+
+    it('Should divide two numbers', () =>{
+        expect(divide(3, 2)).toBe(1.5);
+    });
+
+    it('Should multiply two numbers', () =>{
+        expect(multiply(3, 2)).toBe(6);
+    });
+
+    it('Should power two numbers', () =>{
+        expect(power(3, 2)).toBe(9);
+    });
+
+    it('Should modulo two numbers', () =>{
+        expect(modulo(3, 2)).toBe(1);
     });
 })
